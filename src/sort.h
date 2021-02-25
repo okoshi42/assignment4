bd17ac86a325623e4e59896224c9534b92567352
Jacob Okoshi <jacob.okoshi@mymail.champlain.edu>
2021-02-25 12:07:57 -0500

    completed assignment4Answers

--- src/sort.h
+++ src/sort.h
@@ -39,13 +39,30 @@ namespace csi281 {
     template <typename T>
     void bubbleSort(T array[], const int length) {
         // YOUR CODE HERE
+        for (int x = 0; x < length - 1; x++)//shrinks search area by one each iteration
+        {
+            for (int y = 0; y < (length - x - 1); y++)//at each index y, compares against the index directly after y
+                if (array[y] > array[y + 1]) //swaps if index y is greater than index y+1
+                    swap(array[y], array[y + 1]);
+        }
+
     }
     
     // Performs an in-place ascending sort of *array* of size *length*
     // using the selection sort algorithm
     template <typename T>
     void selectionSort(T array[], const int length) {
+       
         // YOUR CODE HERE
+        for (int x = 0; x < length - 1; x++)//pushes the swapping index up by one each iteration
+        {
+            int minVal = x; //index of lowest value, initialized at x;
+            for (int y = x + 1; y < length; y++) {//at each index after x, compares current index against minVal
+                if (array[y] < array[minVal])//updates minVal if current index is less than minVal
+                    minVal = y;
+            }
+            swap(array[minVal], array[x]);//swaps x with the lowest value found in the unsorted section of the array
+        }
     }
     
     // Performs an in-place ascending sort of *array* of size *length*
@@ -53,6 +70,20 @@ namespace csi281 {
     template <typename T>
     void insertionSort(T array[], const int length) {
         // YOUR CODE HERE
+        for (int x = 1; x < length; x++) {
+            T targetVal = array[x]; //value to be compared against, initialized at index x
+            //checks every value left of x
+            int y = x - 1; 
+
+            // 1 4 2 5 3
+            while (y >= 0 && array[y] > targetVal) { //if the current index is larger than targetVal, the loop breaks
+                //shifts each value to the right by one
+                array[y + 1] = array[y]; 
+                y--;
+            }
+            //inserts targetVal into index left of the last index shifted
+            array[y + 1] = targetVal;
+        }
     }
 }
 
